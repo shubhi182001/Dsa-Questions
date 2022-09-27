cc8cd0a6f29e359e32b87081108e596ee72863d0
Shubhi Kedia <kediashubhi@gmail.com>
2022-09-27 23:21:24 +0530

    Time: 6 ms (49.22%), Space: 7.7 MB (55.37%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,29 +1,35 @@
 class Solution {
 private:
-    void recurPermute(vector<int>& nums, vector<vector<int>>& ans, vector<int> ds,int freq[] ){
-        if(ds.size() == nums.size()){
-            ans.push_back(ds);
-            return;
+    void recurPermute(int index,vector<int>& nums, vector<vector<int>>& ans){
+        // if(ds.size() == nums.size()){
+        //     ans.push_back(ds);
+        //     return;
+        // }
+        if(index ==nums.size()){
+            ans.push_back(nums);
         }
-        for(int i =0 ;i<nums.size(); i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i] = 1;
-                recurPermute( nums, ans, ds, freq );
-                freq[i] = 0;
-                ds.pop_back();
-            }
+        for(int i =index ;i<nums.size(); i++){
+            // if(!freq[i]){
+            //     ds.push_back(nums[i]);
+            //     freq[i] = 1;
+            //     recurPermute( nums, ans, ds, freq );
+            //     freq[i] = 0;
+            //     ds.pop_back();
+            // }
+            swap(nums[index], nums[i]);
+            recurPermute(index+1, nums, ans);
+            swap(nums[index], nums[i]);
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int> ds;
-        int freq[nums.size()];
-        for(int i = 0; i<nums.size(); i++){
-            freq[i] = 0;
-        }
-        recurPermute(nums, ans , ds , freq);
+        // vector<int> ds;
+        // int freq[nums.size()];
+        // for(int i = 0; i<nums.size(); i++){
+        //     freq[i] = 0;
+        // }
+        recurPermute(0,nums, ans );
         return ans;
     }
 };
